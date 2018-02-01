2da9b572cc2757e281b0887c53e4fdb9d6ad908e
freddy vaca <dpdroles.arnoldclark@gmail.com>
2018-02-01 11:18:16 +0000

    Modifications of February 1, 2018

--- dpdrole/dpdrole/Models/Models.cs
+++ dpdrole/dpdrole/Models/Models.cs
@@ -19,28 +19,41 @@ namespace dpdrole.Models
         // completion last month add a £20 completion fee
 
         [Required]
+        [MaxLength(5)]
+        [Range(0, 99999)]
         [Display(Name = "Vehicle price")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter only numeric number")]
         public int Price { get; set; }
 
         [Required]
+        [MaxLength(5)]
+        [Range(0, 99999)]
         [Display(Name = "Deposit amount (minimum 15%)")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter only numeric number")]
         public int Deposit { get; set; }
 
         [Required]
         [Display(Name = "Delivery date")]
-
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
         public DateTime? DeliveryDate { get; set; }
 
         [Required]
-        [Display(Name = "Years finance")]
+        [Range(1, 3)]
+        [Display(Name = "Years of financing")]
         public int Years { get; set; }
 
         [Required]
+        [MaxLength(3)]
+        [Range(0, 999)]
         [Display(Name = "Arrangement fee (Added to the first month)")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter only numeric number")]
         public int Arrangement { get; set; }
 
         [Required]
+        [MaxLength(3)]
+        [Range(0, 999)]
         [Display(Name = "Completion fee (Added to the last month)")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter only numeric number")]
         public int Completion { get; set; }
     }
 }
